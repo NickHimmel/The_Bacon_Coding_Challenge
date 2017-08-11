2195bb86d6db45c2dc91b1cd9ad21937669440ce
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-11 16:42:58 -0400

    Write validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,4 @@
 class Job < ApplicationRecord
+  validates :title, :company, :level, salary:, description:, :city, :state, presence: true
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  validates :username, :email, :password, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 6}
+  has_secure_password
+  before_validation { self.password_confirmation ||= password }
+  
+  has_many :jobs
 end
