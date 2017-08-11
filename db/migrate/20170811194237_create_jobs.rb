a87a690000d31efbd0315c99d3d06aa19b8b68ef
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-11 15:43:47 -0400

    Add Job model

---
+++ db/migrate/20170811194237_create_jobs.rb
@@ -0,0 +1,14 @@
+class CreateJobs < ActiveRecord::Migration[5.0]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :company
+      t.string :level
+      t.integer :salary
+      t.text :description
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
