f17f3d40614cfb797f4533dba9230014c74321b1
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-12 14:11:20 -0400

    Write rspec tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,9 @@ require 'spec_helper'
 
 RSpec.describe User, type: :model do
   it { should have_many(:jobs) }
-  # it { should validate_presence_of(:username) }
-  # it { should validate_presence_of(:email) }
-  # it { should validate_presence_of(:password) }
-  # it { should validate_uniqueness_of(:username) }
-  # it { should validate_length_of(:password).is_at_least(6)}
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password) }
+  it { should validate_uniqueness_of(:username) }
+  it { should validate_length_of(:password).is_at_least(6)}
 end
