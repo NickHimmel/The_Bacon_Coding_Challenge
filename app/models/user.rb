6f5a8f583c5b43db1d7263356c930652465212ef
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-12 14:14:03 -0400

    Pass rspec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # validates :username, :email, :password, presence: true
-  # validates :username, uniqueness: true
-  # validates :password, length: {minimum: 6}
-  # has_secure_password
-  # before_validation { self.password_confirmation ||= password }
+  validates :username, :email, :password, presence: true
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 6}
+  has_secure_password
+  before_validation { self.password_confirmation ||= password }
 
-  # has_many :jobs
+  has_many :jobs
 end
