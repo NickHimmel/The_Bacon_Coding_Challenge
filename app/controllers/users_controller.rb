b21d8e75fffa54f2fbf8583fffc4a5cff2172ada
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-13 19:13:18 -0400

    Set user routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def create
+    @user = User.create!(user_params)
+    json_response(@user, :created)
+  end
+
+  def show
+    json_response(@user)
+  end
+
+  private
+
+  def user_params
+    params.permit(:username, :email, :password)
+  end
 end
