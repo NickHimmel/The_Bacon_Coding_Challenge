d775b9a8211779241eab0dd3f49d244e27532ea9
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-14 16:07:55 -0400

    Add namespace and version control for api

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,5 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < Api::V1::BaseController
+  
   before_action :set_user, only: [:show]
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'site#index'
-  resources :jobs do
-  end
-  resources :users, except: :destroy do
+  namespace :api do
+    namespace :v1 do
+      resources :jobs do
+      end
+      resources :users, except: :destroy do
+      end
+    end
   end
 end
