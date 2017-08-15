96a56ec62a1f4e820b42364b4ca6d193009cb897
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-15 13:56:56 -0400

    Add routes for react routes

--- app/assets/javascripts/components/routes.js.jsx
+++ app/assets/javascripts/components/routes.js.jsx
@@ -1,4 +1,20 @@
-var React = require('react');
-var Router = require('react-router');
-var Route = Router.Route;
-var DefaultRoute = Router.DefaultRoute;
+let Router = ReactRouter.Router;
+let Route = ReactRouter.Route;
+let Link = ReactRouter.Link;
+let IndexRoute = ReactRouter.IndexRoute;
+let IndexLink = ReactRouter.IndexLink;
+let IndexRedirect = ReactRouter.IndexRedirect;
+let hashHistory = ReactRouter.hashHistory;
+let browserHistory = ReactRouter.browserHistory;
+
+class Routes extends React.Component {
+  render() {
+    return (
+      <Router histroy={hashHistory}>
+        <Route path='/' component={Main}>
+          <IndexRoute component={all_jobs}/>
+        </Route>
+      </Router>
+    )
+  }
+}
