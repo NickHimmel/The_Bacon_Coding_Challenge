c749e3e817b979bc519b66df8204467097ff5008
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-16 15:47:32 -0400

    Add more info to the job listing component

--- app/assets/javascripts/components/job_listing.js.jsx
+++ app/assets/javascripts/components/job_listing.js.jsx
@@ -1,10 +1,11 @@
 const JobListing = (props) => {
   return (
-    <div>
-      <a hfref="/job/:id">
-      </a>
-      <h2 className="text-center">{props.job.title}</h2>
-      <h3 className="text-center">{props.job.company}</h3>
+    <div col-xs-12 col-sm-12 col-md-12>
+      <h2 className="job-title">{props.job.title}</h2>
+      <h3 className="job-company">{props.job.company}</h3>
+      <p className="city">{props.job.city}</p>
+      <p className="state">{props.job.state}</p>
+      <p className="description">{props.job.description}</p>
     </div>
   )
 }
