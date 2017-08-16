bd01024f00c77d4e9287ec5cab1e4a634e15fdad
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-16 11:08:56 -0400

    update job listing

--- app/assets/javascripts/components/job_listing.js.jsx
+++ app/assets/javascripts/components/job_listing.js.jsx
@@ -1,6 +1,8 @@
 const JobListing = (props) => {
   return (
     <div>
+      <a hfref="/job/:id">
+      </a>
       <h2 className="text-center">{props.job.title}</h2>
       <h3 className="text-center">{props.job.company}</h3>
     </div>
