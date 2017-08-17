742429c012f2cd920943504dc9c6bc0ef2270498
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-17 14:10:37 -0400

    Add map icon

--- public/assets/components/job-f56913d71379ae8d8ebc634382e01f35259bca062b8ad5d356b2572dee6cdc09.jsx
+++ public/assets/components/job-f56913d71379ae8d8ebc634382e01f35259bca062b8ad5d356b2572dee6cdc09.jsx
@@ -3,7 +3,7 @@ const Job = (props) => {
     <div>
       <h2 className="job-title">{props.job.title}</h2>
       <p className="job-company">{props.job.company}</p>
-      <img src="https://s3.us-east-2.amazonaws.com/nickhimmel.com/Map_pin_icon.svg" className="map-icon"/>
+      <img src="assets/images/Map_pin_icon.svg"/>
       <p className="city">{props.job.city}</p>
       <p className="state">{props.job.state}</p>
       <p className="description">{props.job.description}</p>
