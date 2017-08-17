69ece8e9056f8d209d0800a9189a40e0b77065f6
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-17 08:54:05 -0400

    Add welcome message

---
+++ public/assets/components/welcome-b6fa48b2feefd31d0e196cea9048b092208bc0663a233a5a0a24015a5795e121.jsx
@@ -0,0 +1,8 @@
+const Welcome = (props) => {
+  return (
+    <div className="welcome-banner">
+      <h1 className="welcome-message">Welcome to Prosciutto!</h1>
+      <h2>Bring home the finest bacon!</h2>
+    </div>
+  )
+}
---
+++ public/assets/components/welcome-bdbf9309786f08db385b7031943cd0ab5dd6179cd5aabf2b6a21bb9ef1a12399.jsx
@@ -0,0 +1,7 @@
+const Welcome = (props) => {
+  return (
+    <div className="welcome-banner">
+      <p>Welcome to Prosciutto!</p>
+    </div>
+  )
+}
