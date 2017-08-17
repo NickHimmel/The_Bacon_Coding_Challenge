388973d812a742a1d21774d006ae390a4ee53347
[01;31m[K"name"[m[K: "Nick Himmel <[01;31m[K"email"[m[K: null>
2017-08-17 15:47:15 -0400

    Update welcome banner with sass to practice sass

--- public/assets/components/welcome-1cb879ad61ee710d2a514ca8f4e950ca7383d08f2d3304b67764ef366a58a175.jsx
+++ public/assets/components/welcome-1cb879ad61ee710d2a514ca8f4e950ca7383d08f2d3304b67764ef366a58a175.jsx
@@ -2,6 +2,7 @@ const Welcome = (props) => {
   return (
     <div className="welcome-message">
       <h1>Welcome to Prosciutto!<span className="tagline"> Bring home the finest bacon</span></h1>
+      {/* <h2>Bring home the finest bacon</h2> */}
       <p>Find your dream job on Prosciutto, the best job board on the whole internet. Look around and you will see that these jobs are top-notch.</p>
     </div>
   )
--- public/assets/components/welcome-9f0171b3131f7aca0803763760af54d636ea8fc380a564337067d7696121774c.jsx
+++ public/assets/components/welcome-9f0171b3131f7aca0803763760af54d636ea8fc380a564337067d7696121774c.jsx
@@ -2,7 +2,8 @@ const Welcome = (props) => {
   return (
     <div className="welcome-message">
       <h1>Welcome to Prosciutto!<span className="tagline"> Bring home the finest bacon</span></h1>
-      <p>Find your dream job on Prosciutto, the best job board on the whole internet. Look around and you will see that these jobs are top-notch.</p>
+      {/* <h2>Bring home the finest bacon</h2> */}
+      <p>Find your dream job on Prosciutto, the best job board on the internet.</p>
     </div>
   )
 }
